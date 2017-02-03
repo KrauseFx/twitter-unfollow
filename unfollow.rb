8360da11ce8cbba4e602ef9150055eb37284d70d
Felix Krause <krausefx@gmail.com>
2017-02-02 17:27:09 -0800

    Add unfollow.rb
    Fixes https://github.com/KrauseFx/twitter-unfollow/issues/1

---
+++ unfollow.rb
@@ -0,0 +1,31 @@
+require 'twitter'
+
+class Unfollow
+  def run
+    followings = client.following
+
+    followings.each do |user|
+      puts "Unfollowing user #{user.screen_name} (#{user.name})"
+      client.add_list_member(old_follows_list, user.id)
+      client.unfollow(user.id)
+      `echo "#{user.screen_name}" >> unfollowed_usernames.txt`
+      `echo "#{user.screen_name} (#{user.name}): #{user.description}" >> unfollowed_full_names.txt`
+      sleep 1
+    end
+  end
+
+  def old_follows_list
+    @_old_follows_list ||= client.lists.find { |a| a.name == "Old Follows" }
+  end
+
+  def client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    end
+  end
+end
+
+Unfollow.new.run
